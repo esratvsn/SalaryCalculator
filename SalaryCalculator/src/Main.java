7ab1a754b8baaba5556d7562369afa2a973c0ea3
esratavsan <2110213057@ogrenci.karabuk.edu.tr>
2024-07-31 21:07:25 +0300

    Add files via upload

---
+++ SalaryCalculator/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Esra", 2000, 45, 1985);
+        System.out.println(emp1.toString());
+
+    }
+}
